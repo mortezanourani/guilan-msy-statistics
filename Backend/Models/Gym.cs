d29270ba01f2202e7e02486cc9f0449fa47a187e
Morteza Nourani <mortezanourani.gmail.com>
2025-01-02 12:46:33 +0330

    Add Gym model

---
+++ Backend/Models/Gym.cs
@@ -0,0 +1,17 @@
+﻿namespace Backend.Models;
+
+public class Gym
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public virtual City City { get; set; }
+    public string Address { get; set; }
+    public string PostalCode { get; set; }
+    public string Owner { get; set; }
+    public virtual BuildingOwnership BuildingOwnership { get; set; }
+    public string LicenseNumber { get; set; }
+    public DateOnly LicenseStart { get; set; }
+    public DateOnly LicenseExpiration { get; set; }
+    public virtual LicenseOwnership LicenseOwnership { get; set; }
+    public virtual Gender Gender { get; set; }
+}
