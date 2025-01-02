3101c6d934eee74999178350a9783fbd9be611ea
Morteza Nourani <mortezanourani.gmail.com>
2025-01-02 13:02:10 +0330

    Add DbContext

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,14 @@
+using Backend.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
